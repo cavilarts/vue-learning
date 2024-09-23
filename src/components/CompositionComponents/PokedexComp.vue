e057a7a712e1e1204e7a307828a5c7a1800d7634
Carlos Avila <cavilart21@gmail.com>
2024-09-23 16:43:28 +0200

    computed values, references and reactive

--- src/components/CompositionComponents/PokedexComp.vue
+++ src/components/CompositionComponents/PokedexComp.vue
@@ -1,7 +1,17 @@
 <script>
+import { reactive } from 'vue'
+import { computed } from 'vue'
+import { ref } from 'vue'
+
 export default {
   async setup() {
-    const region = 'Kanto'
+    const region = ref('Kanto')
+    const regionAllCaps = computed(() => {
+      return region.value.toUpperCase()
+    })
+    const types = reactive({
+      favoriteType: 'Venom'
+    })
 
     const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((res) =>
       res.json()
@@ -9,11 +19,32 @@ export default {
 
     return {
       pokedex,
-      region
+      region,
+      regionAllCaps,
+      types
+    }
+  },
+  methods: {
+    updateRegion() {
+      if (this.region === 'Kanto') {
+        this.region = 'Hoenn'
+      } else {
+        this.region = 'Kanto'
+      }
+    }
+  },
+  computed: {
+    regionNameLowerCase() {
+      return this.region.toLowerCase()
     }
   }
 }
 </script>
 <template>
+  <button @click="updateRegion">Update Region</button>
+  <h2>{{ region }}</h2>
+  <h3>{{ regionAllCaps }}</h3>
+  <h3>{{ regionNameLowerCase }}</h3>
+  <p>{{ types.favoriteType }}</p>
   <pre>{{ pokedex }}</pre>
 </template>
