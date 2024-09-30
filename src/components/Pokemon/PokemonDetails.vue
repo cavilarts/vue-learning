36cf4c67b0432f2afa9b4604f8d922c9e0933970
Carlos Avila <cavilart21@gmail.com>
2024-09-30 09:32:45 +0200

    adding missing view and component to display pokemon details

---
+++ src/components/Pokemon/PokemonDetails.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useRoute } from 'vue-router'
+
+const router = useRoute()
+
+const pokemonDetails = await fetch(`https://pokeapi.co/api/v2/pokemon/${router.params.name}`).then(
+  (res) => res.json()
+)
+</script>
+<template>
+  <h2>{{ pokemonDetails.name }}</h2>
+  <img
+    :src="pokemonDetails.sprites.front_default"
+    :alt="pokemonDetails.name"
+    width="200"
+    height="auto"
+  />
+</template>
---
+++ src/views/PokemonView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import PokemonDetails from '@/components/Pokemon/PokemonDetails.vue'
+</script>
+<template>
+  <Suspense>
+    <PokemonDetails />
+    <template #fallback> Loading... </template>
+  </Suspense>
+</template>
